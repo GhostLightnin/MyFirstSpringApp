651ec17b741f9c6ffe02c5122d99abc84e4b4024
GhostLightnin <48833958+GhostLightnin@users.noreply.github.com>
2022-10-04 16:39:41 +0300

    Initial commit

---
+++ src/main/java/com/example/MyFirstSpringApp/Controllers/MainPageController.java
@@ -0,0 +1,21 @@
+package com.example.MyFirstSpringApp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainPageController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
+}
